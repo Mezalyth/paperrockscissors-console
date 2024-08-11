2a71ce67f81ec46730d4c5e34652cfd9c2e408f3
Mezalyth <86799855+Mezalyth@users.noreply.github.com>
2024-08-11 00:27:22 -0700

    Restructure to avoid double-prompts, add round comparison

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 console.log("Hello, world!");
-
-//Get user choice
-function getUserChoice() {
-    let choice = window.prompt("Rock, Paper, or Scissors?");
-    choice = choice.toLowerCase();
-    if (choice != 'rock' && choice != 'paper' && choice != 'scissors') {
-        alert("Invalid choice! Try again.");
-        return getUserChoice;
-    } else {
-        console.log("User Choice: " + choice);
-        return choice;
-    }
-}
-getUserChoice();
+let humanScore = 0;
+let computerScore = 0;
 
 //Get computer choice
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let computerChoice = Math.floor(Math.random() * 3);
     return choices[computerChoice];
-    console.log(computerChoice);
 }
-console.log("Computer Choice: " + getComputerChoice());
+
+//Get user userChoice
+function getUserChoice() {
+    let userChoice = window.prompt("Rock, Paper, or Scissors?");
+    userChoice = userChoice.toLowerCase();
+    if (userChoice != 'rock' && userChoice != 'paper' && userChoice != 'scissors') {
+        alert("Invalid userChoice! Try again.");
+        return getUserChoice();
+    } else {
+        return userChoice;
+    }
+}
+//Compare choices and determine winner
+function playRound(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        alert("Draw! Play again, you got this!");
+        return;
+    }
+
+    if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'scissors' && computerChoice === 'paper') ||
+        (userChoice === 'paper' && computerChoice === 'rock')
+    ) {
+        alert("You win this round!");
+        humanScore ++;
+    } else {
+        alert("Computer won this round.");
+        computerScore ++;
+    }
+}
+const humanSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log("Computer Choice: " + computerSelection);
+console.log("Human Choice: " + humanSelection);
+console.log("Computer Score: " + computerScore);
+console.log("Human Score: " + humanScore);
