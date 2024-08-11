6f69799e73417f8ab276f64e218eb17adb9e0fee
Mezalyth <86799855+Mezalyth@users.noreply.github.com>
2024-08-10 23:23:38 -0700

    Prompt and store user choice.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 console.log("Hello, world!");
 
+//Get user choice
+function getUserChoice() {
+    let choice = window.prompt("Rock, Paper, or Scissors?");
+    choice = choice.toLowerCase();
+    if (choice != 'rock' && choice != 'paper' && choice != 'scissors') {
+        alert("Invalid choice! Try again.");
+        return getUserChoice;
+    } else {
+        console.log("User Choice: " + choice);
+        return choice;
+    }
+}
+getUserChoice();
+
 //Get computer choice
-function getComputerChoice()
-{
-  let choices = ["rock", "paper", "scissors"];
-  let computerChoice = Math.floor(Math.random() * 3);
-  return choices[computerChoice];
-  console.log(computerChoice);
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let computerChoice = Math.floor(Math.random() * 3);
+    return choices[computerChoice];
+    console.log(computerChoice);
 }
 console.log("Computer Choice: " + getComputerChoice());
